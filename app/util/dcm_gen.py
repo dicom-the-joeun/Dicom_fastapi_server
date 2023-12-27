dc944557786edcd05203f9dc06021b07f86853fd
Sumin, Kim <mykkang55@naver.com>
2023-12-27 20:54:52 +0900

    mod Dicom_gen
    - FTP에서 받은 Binary data를 JSON 형식으로 변환

--- app/util/dcm_gen.py
+++ app/util/dcm_gen.py
@@ -1,108 +1,49 @@
+'''
+    Convert Dicom
+    FTP -> binarydata -> ds -> toJSON
+    Description : FTP에서 받은 binary data -> ds로 JSON 직렬화
+    Author : Okrie
+    Ver : 0.1
+    Site : https://github.com/Okrie/DicomToPng
+    Lisence : MIT
+'''
+
 import matplotlib.pyplot as plt
-from pydicom import dcmread, multival
 import numpy as np
-import base64
-from pydicom import dcmread
-from ftplib import FTP
-from conf.ftp_config import FTPConfig
+import base64, io
+from pydicom import dcmread, multival
 
 
-# FTB Config Load
-CONFTP = FTPConfig()
-
-"""
-    TODO : 
-    1) .dcm -> .JSON
-     ** 단일 책임, 구분을 위해, FTP 연결은 dcm_service 진행.
-     ** 앞단, 데이터 전송은 여기서 하는 것이 아닌, dcm_ctrl.
-    
-    ## 일단, 의존성 설치는 끝마쳤고, DICOMTOPNG proj에서 Method 만 복붙함.
-"""
-
-class recontrol:
-    
-    def __init__(self):
-        CONFTP.connect()
-    
-    # byte to ndarray
-    @staticmethod
-    def Recontrol(co, arrdType, arrShape):
-        """
-            ### Recontrol
-            Reshape Array
-        """
-        
-        convertArray = np.frombuffer(co, dtype=arrdType)
-        arrRes = np.reshape(convertArray, arrShape)
-        return arrRes
-
+class ConvertDCM:
+    '''
+        Convert Dicom
+        FTP -> binarydata -> ds -> toJSON
+    '''
     # Convert Pixel data
-    @staticmethod
-    def convertPixel(filename, type='b'):
+    def dicomToJSON(self, data):
         """
             ### convertPixel
             require input paths = FTP folder paths
         """
+        ds = dcmread(io.BytesIO(data))
+        keys = list(ds.keys())
+        ds_keys = []
+        result = '{'
+
+        for v in keys:
+            ds_keys.append(str(v).replace('(', '0x').replace(', ', '').replace(')', ''))
+
+        for i, v in enumerate(ds):
+            if i < len(keys) - 1:
+                result = result + f'"{ds[ds_keys[i]].name}" : "{ds[ds_keys[i]].value}" , '
+            else:
+                result = result + f'"{ds[ds_keys[i]].name}" : "{base64.b64encode(self.loadData(ds))}"'
+        result = result + "}"
         
-        '''
-            TODO:
-            filename을 FTP 형식으로 불러와야 함
-        '''
-        
-        data = ''
-        f = CONFTP.getFTP()
-        f.retrbinary(f'RESP {filename}', data)
-        if f:
-            ds = dcmread(data)
-            
-            keys = list(ds.keys())
-            ds_keys = []
-            result = '{'
-
-            for v in keys:
-                ds_keys.append(str(v).replace('(', '0x').replace(', ', '').replace(')', ''))
-
-            for i, v in enumerate(ds):
-                if i < len(keys) - 1:
-                    result = result + f'"{ds[ds_keys[i]].name}" : "{ds[ds_keys[i]].value}" , '
-                else:
-                    result = result + f'"{ds[ds_keys[i]].name}" : "{base64.b64encode(ds[ds_keys[i]].value)}"'
-            result = result + "}"
-
-        f.close()
         return result
-
-
-class viewDCM:
-    @staticmethod
-    def viewDCM(filename, arr):
-        """
-        ### View Dicom Image by MatPlotLib
-        """
-        plt.title(filename)
-        plt.imshow(arr, cmap='gray')
-        plt.show()
-
-    # Save Pixel_data to Image File
-
-    @staticmethod
-    def saveTopng(filename, arr, dpi=500, type='png'):
-        """
-        ### Dcm Image to Png    
-        require : filename, arr    
-        arr : dicom pixel_data    
-        default dpi : 1000    
-        default type : png    
-        """
-
-        plt.axis('off')
-        plt.imshow(arr, cmap='gray')
-        plt.savefig(f'{filename}.{type}', dpi=dpi, transparent=True)
-
+    
     # Load pixel_data with Dicom Header
-
-    @staticmethod
-    def loadFile(filename, i):
+    def loadData(self, data):
         """
         ### DCM Convert pixel_data      
         require : fileName, i      
@@ -110,7 +51,7 @@ class viewDCM:
         information : Check Header data and convert pixel_data
         """
 
-        ds = dcmread(filename + '.dcm', force=True)
+        ds = dcmread(io.BytesIO(data))
         if ds is None:
             raise Exception('File Exits')
         pixel_array = ds.pixel_array
@@ -154,7 +95,4 @@ class viewDCM:
             pixel_array[np.where(pixel_array > rwin)] = rwin
             pixel_array = pixel_array - lwin
 
-        if i == 1:
-            return pixel_array
-        elif i == 2:
-            return ds
+        return pixel_array
