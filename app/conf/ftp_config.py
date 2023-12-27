0997d538765bea006bc84344e65725852b01a1d2
Okrie <mykkang55@naver.com>
2023-12-28 02:14:17 +0900

    add test code
    - local 파일 읽은 데이터와 ftp에서 받은 데이터 같은지 확인 테스트 함수

--- app/conf/ftp_config.py
+++ app/conf/ftp_config.py
@@ -41,8 +41,47 @@ class FTPConfig:
         # data = file_data.getvalue()
         # print(type(data))
         # return data
-        return file_data
+
+        # getvalue()로 바꿈 테스트 필요
+        return file_data.getvalue()
 
     def disconnect(self):
         self.ftp.quit()
         print("Disconnected from FTP server")
+
+
+    # local과 ftp 데이터 같은지 확인용 테스트 함수
+    def getdataTestCode(self, filepath, filename):
+        '''
+            TODO:
+            두개 다 동일한 파일로 진행
+            Local 파일은 최상단에 위치 시킨다.
+            인자 filepath 는 ftp에서만 사용하며 filename은 공용으로 받는다.
+        '''
+        from pydicom import dcmread
+        
+        # local file
+        local_path = f'./{filename}'
+        with open(local_path, 'rb') as file:
+            local_data = file.read()
+
+        # ftp file
+        file_data = BytesIO()
+        self.ftp.retrbinary(f'RETR {filepath}', file_data.write)
+        ftp_data = file_data.getvalue()
+
+        # DICOM 파일 해석 및 비교
+        local_dicom = dcmread(BytesIO(local_data))
+        ftp_dicom = dcmread(BytesIO(ftp_data))
+
+        # 메타데이터 비교
+        if local_dicom == ftp_dicom:
+            print("메타데이터 일치")
+        else:
+            print("메타데이터 불일치")
+
+        # 이미지 데이터 비교
+        if local_dicom.pixel_array.tobytes() == ftp_dicom.pixel_array.tobytes():
+            print("이미지 데이터 일치")
+        else:
+            print("이미지 데이터 불일치")
