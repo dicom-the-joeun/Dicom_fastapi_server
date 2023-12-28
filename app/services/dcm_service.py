6aac01b57e51177fc120c44264c0ead9cf4f6504
Oh-Kang94 <okh19941994@naver.com>
2023-12-28 15:26:16 +0900

    sub comments & add gitignore pattern

--- app/services/dcm_service.py
+++ app/services/dcm_service.py
@@ -8,11 +8,9 @@ ftp = FTPConfig()
 class DcmService:
     @staticmethod
     def get_dcm(filepath, filename):
-        # filepath = f'/{filename}'
         conv = ConvertDCM()
         try:
             ftp.connect()
-            print(filepath)
             data = ftp.getdata(filepath=filepath, filename=filename)
         except Exception as e:
             print(f'문제는 {e}')
