d1459b33275b72d300180a98238f4723a932f4ce
Oh-Kang94 <okh19941994@naver.com>
2024-01-02 17:39:38 +0900

    add get_front fn

--- app/util/dcm_gen.py
+++ app/util/dcm_gen.py
@@ -8,6 +8,9 @@
     Lisence : MIT
 '''
 
+from PIL import Image
+import pydicom
+import io
 import json
 import logging
 import matplotlib.pyplot as plt
@@ -26,130 +29,59 @@ from pydicom import dcmread
     ## 일단, 의존성 설치는 끝마쳤고, DICOMTOPNG proj에서 Method 만 복붙함.
 """
 
-class recontrol:
-    
-    # byte to ndarray
-    @staticmethod
-    def Recontrol(co, arrdType, arrShape):
+'''
+    Convert Dicom
+    FTP -> binarydata -> ds -> toJSON
+    Description : FTP에서 받은 binary data -> ds로 JSON 직렬화
+    Author : Okrie, Oh-Kang94
+    Ver : 0.1
+    Site : https://github.com/Okrie/DicomToPng
+    Lisence : MIT
+'''
+
+
+class ConvertDCM:
+    '''
+        Convert Dicom
+        FTP -> binarydata -> ds -> toJSON
+    '''
+
+    def dicomToJSON(self, data):
+        ds = pydicom.dcmread(io.BytesIO(data))
+        # Number of Frames가 있으면 [0]만 가져온다.
+        try:
+            pixel_array = ds.pixel_array[0]
+            image = Image.fromarray(pixel_array)
+            buffered = io.BytesIO()
+            image.save(buffered, format="PNG")
+            png_bytes = buffered.getvalue()
+            base64_encoded = base64.b64encode(png_bytes).decode('utf-8')
+        except Exception as e:
+            logging.error(f"이미지 문제 발생 {e}")
+            base64_encoded = "None"
+        finally:
+            result = {}
+            for elem in ds:
+                if elem.name == "Pixel Data":
+                    result[elem.name] = base64_encoded
+                else:
+                    result[elem.name] = str(elem.value)
+            return json.dumps(result)
+
+            # return self.get_front(ds, base64_encoded)
+
+    def get_front(self, ds, base64_encoded):
         """
-            ### Recontrol
-            Reshape Array
+        ### Front Info for list
+        @Params : fname -> SC 판단 후, Slice Score를 뱉어냄
         """
-        
-        convertArray = np.frombuffer(co, dtype=arrdType)
-        arrRes = np.reshape(convertArray, arrShape)
-        return arrRes
+        result = {}
+        info_list = ["Patient ID", "Patient's Name", "Patient's Birth Date", "Series Number", "Study Date", "Study Time", "Image Comments",
+                     "Manufacturer", "Manufacturer's Model Name", "Rows", "Columns", "Window Width", "Window Center", "Operator's Name"]
 
-    # Convert Pixel data
-    @staticmethod
-    def convertPixel(filename, type='b'):
-        """
-            ### convertPixel
-        """
-
-        paths = os.path.abspath(os.path.dirname(os.path.dirname(__file__))) + '/'
-        
-        if(type == 'b'):
-            f = open(paths+filename+'.dcm', 'rb')
-            ds = bytes(f.read())
-            f.close()
-
-            f = open(paths+filename+'.txt', 'w')
-            f.write(str(ds))
-            f.close()
-        else:
-            ds = dcmread(paths + filename + '.dcm')
-            f = open(paths + filename + '.json', 'w')
-            f.write(str(ds.to_json()))
-            f.write(str(','))
-            f.write(str({'base64' : base64.b64encode(ds.pixel_array)}).replace("'", '"'))
-            f.close()
-
-        return ds
-
-
-class viewDCM:
-    @staticmethod
-    def viewDCM(filename, arr):
-        """
-        ### View Dicom Image by MatPlotLib
-        """
-        plt.title(filename)
-        plt.imshow(arr, cmap='gray')
-        plt.show()
-
-    # Save Pixel_data to Image File
-
-    @staticmethod
-    def saveTopng(filename, arr, dpi=500, type='png'):
-        """
-        ### Dcm Image to Png    
-        require : filename, arr    
-        arr : dicom pixel_data    
-        default dpi : 1000    
-        default type : png    
-        """
-
-        plt.axis('off')
-        plt.imshow(arr, cmap='gray')
-        plt.savefig(f'{filename}.{type}', dpi=dpi, transparent=True)
-
-    # Load pixel_data with Dicom Header
-
-    @staticmethod
-    def loadFile(filename, i):
-        """
-        ### DCM Convert pixel_data      
-        require : fileName, i      
-        i : 1 (pixel_data), 2 (Dicom Header)     
-        information : Check Header data and convert pixel_data
-        """
-
-        ds = dcmread(filename + '.dcm', force=True)
-        if ds is None:
-            raise Exception('File Exits')
-        pixel_array = ds.pixel_array
-
-        img = ds.pixel_array.astype(np.float32)
-
-        # Noramalization
-        img = (img / (2 ** ds.BitsStored))
-
-        # Convert Rescale
-        if (('RescaleSlope' in ds) and ('RescaleIntercept' in ds)):
-            pixel_array = (pixel_array * ds.RescaleSlope) + ds.RescaleIntercept
-
-        if ('WindowCenter' in ds):
-            if (type(ds.WindowCenter) == multival.MultiValue):
-                window_center = float(ds.WindowCenter[0])
-                window_width = float(ds.WindowWidth[0])
-                lwin = window_center - (window_width / 2.0)
-                rwin = window_center + (window_width / 2.0)
-            else:
-                window_center = float(ds.WindowCenter)
-                window_width = float(ds.WindowWidth)
-                lwin = window_center - (window_width / 2.0)
-                rwin = window_center + (window_width / 2.0)
-        else:
-            lwin = np.min(pixel_array)
-            rwin = np.max(pixel_array)
-
-        pixel_array[np.where(pixel_array < lwin)] = lwin
-        pixel_array[np.where(pixel_array > rwin)] = rwin
-        pixel_array = pixel_array - lwin
-
-        if (ds.PhotometricInterpretation == 'MONOCHROME1'):
-            pixel_array[np.where(pixel_array < lwin)] = lwin
-            pixel_array[np.where(pixel_array > rwin)] = rwin
-            pixel_array = pixel_array - lwin
-            pixel_array = 1.0 - pixel_array
-
-        else:
-            pixel_array[np.where(pixel_array < lwin)] = lwin
-            pixel_array[np.where(pixel_array > rwin)] = rwin
-            pixel_array = pixel_array - lwin
-
-        if i == 1:
-            return pixel_array
-        elif i == 2:
-            return ds
+        for elem in ds:
+            if elem.name == "Pixel Data":
+                result[elem.name] = base64_encoded
+            elif elem.name in info_list:
+                result[elem.name] = str(elem.value)
+        return json.dumps(result)
