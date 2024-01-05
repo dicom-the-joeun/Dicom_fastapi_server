c2755e772ee8c462e8d3223d39197a9cacbc5919
Oh-Kang94 <okh19941994@naver.com>
2024-01-05 19:48:09 +0900

    add get_thumbnail & get images

--- app/controller/dcm_ctrl.py
+++ app/controller/dcm_ctrl.py
@@ -1,11 +1,12 @@
+import json
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, StreamingResponse
 from sqlalchemy.orm import Session
 from starlette import status
 
 from app.conf.db_config import DBConfig
-from app.models.api_model import SelectPatient
+from app.models.api_model import SelectThumbnail
 from app.services.dcm_service import DcmService
 
 
@@ -13,16 +14,34 @@ router = APIRouter()
 db = DBConfig()
 
 
-@router.get("/test")
+@router.get("/image")
 async def get_dcm_test(filepath: str, filename: str):
-    patients = DcmService.get_dcm(filepath, filename)
-    if not patients:
+    image = DcmService.get_dcm_img(filepath, filename)
+    if not image:
         raise HTTPException(status_code=404, detail="Patients not found")
-    return JSONResponse(content=patients, status_code=status.HTTP_200_OK)
+    return StreamingResponse(image, media_type="image/png")
 
-@router.get("/")
-async def get_thumbnail(studyid: str, db : Session = Depends(db.get_db)):
-    patients = DcmService.get_dcm(studyid)
-    if not patients:
-        raise HTTPException(status_code=404, detail="Patients not found")
-    return JSONResponse(content=patients, status_code=status.HTTP_200_OK)
+
+@router.get("/thumbnails", response_model=List[SelectThumbnail])
+async def get_thumbnail(studykey: int, db: Session = Depends(db.get_db)):
+    results = []
+    thumbnails = await DcmService.get_dcm_thumbnails(studykey, db)
+    if not thumbnails:
+        raise HTTPException(status_code=404, detail = "Can't find thumbnail")
+    for thumbnail in thumbnails:
+        jsondata = DcmService.get_dcm_json(filepath=thumbnail.PATH,
+                            filename=thumbnail.FNAME)
+        json_dcm = json.loads(jsondata)
+        if "Image Comments" in json_dcm:
+            SCORE = json_dcm["Image Comments"]
+        else:
+            SCORE = None
+        result = SelectThumbnail(
+                    SERIESKEY=thumbnail.SERIESKEY,
+                    SERIESDESC=thumbnail.SERIESDESC,
+                    SCORE=SCORE,
+                    PATH=thumbnail.PATH,
+                    FNAME = thumbnail.FNAME
+                ).dict()
+        results.append(result)
+    return JSONResponse(content=results, status_code=status.HTTP_200_OK)
--- app/services/dcm_service.py
+++ app/services/dcm_service.py
@@ -1,5 +1,7 @@
+import json
 from app.conf.ftp_config import FTPConfig
 from app.models.api_model import SelectThumbnail
+from app.models.db_model import SeriesTab
 from app.util.dcm_gen import ConvertDCM
 from typing import List
 ftp = FTPConfig()
@@ -7,7 +9,7 @@ ftp = FTPConfig()
 
 class DcmService:
     @staticmethod
-    def get_dcm(filepath, filename):
+    def get_dcm_json(filepath, filename):
         conv = ConvertDCM()
         try:
             ftp.connect()
@@ -18,7 +20,21 @@ class DcmService:
             ftp.disconnect()
             return conv.dicomToJSON(data)
         
+
     @staticmethod
-    def get_dcm_thumbnail(db) -> List[SelectThumbnail]:
-        # TODO: thumbnail 빌드 만들었다.
-        thumbnail = db.query()
+    def get_dcm_img(filepath, filename):
+        conv = ConvertDCM()
+        try:
+            ftp.connect()
+            data = ftp.getdata(filepath=filepath, filename=filename)
+        except Exception as e:
+            print(f'문제는 {e}')
+        finally:
+            ftp.disconnect()
+            return conv.dicomToPNG(data)
+
+    @staticmethod
+    async def get_dcm_thumbnails(studykey, db) -> List[SelectThumbnail]:
+        thumbnails = db.query(SeriesTab).filter(
+            SeriesTab.STUDYKEY == studykey).all()
+        return thumbnails
